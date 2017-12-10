074798bceb65f602bc4302970f977c0ba3a136e0
Joshua Wu <Joshua.wu.0910@gmail.com>
2017-12-10 14:08:03 -0800

    Add conditional check for failures. added colors

--- slng.py
+++ slng.py
@@ -5,6 +5,18 @@
 import sys
 import requests
 
+class bcolors:
+    """
+        Add color properties to prnt statements
+    """
+    BOLD = '\033[1m'
+    ENDC = '\033[0m'
+    FAIL = '\033[91m'
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    UNDERLINE = '\033[4m'
+    WARNING = '\033[93m'
 
 def get_word(word):
     """
@@ -20,14 +32,24 @@ def print_definitions(word):
         Loops through first 3 results and prints them out to console
     """
     definitions = get_word(word)
-    for i in range(0, 3):
+
+    if len(definitions) >= 3:
+        defLength = 3
+        print(f'You entered "{word}" and these are the top 3 results:\n')
+    elif len(definitions) == 0:
+        print(bcolors.FAIL + "ERROR: " + bcolors.ENDC + "No entries found, for " + bcolors.HEADER + word + bcolors.ENDC)
+        get_input()
+    else:
+        defLength = len(definitions)
+
+    for i in range(0, defLength):
         obj = definitions[i]
 
-        print(f'Word: {obj["word"]}')
-        print(f'Definition: {obj["definition"]}')
+        print('========================================================')
+        print(f'Word: ' + bcolors.HEADER + str(obj["word"]) + bcolors.ENDC)
+        print(f'Definition: ' + bcolors.HEADER + obj["definition"] + bcolors.ENDC)
         print(f'Score: {obj["thumbs_up"] - obj["thumbs_down"]}')
-        print(f'Ayys: {obj["thumbs_up"]} | Nayys: {obj["thumbs_down"]}\n')
-
+        print('Ayys: ' + bcolors.OKGREEN + str(obj["thumbs_up"]) + bcolors.ENDC + ' | Nayys: ' + bcolors.FAIL + str(obj["thumbs_down"]) + bcolors.ENDC)
 
 def get_input():
     """
@@ -35,17 +57,17 @@ def get_input():
         Gets input string from user and returns 3 definitions.
     """
     while True:
-        words_to_lookup = input('')
+        print('========================================================')
+        words_to_lookup = input('Slng: ')
+
         if not words_to_lookup:
-            words_to_lookup = input(
-                'You didn\'t enter anything.  Please enter a search string: ')
+            print(
+                'You didn\'t enter anything.  Please enter a search string.')
         elif words_to_lookup == 'exit':
             sys.exit()
         elif words_to_lookup:
-            print(f'You entered "{words_to_lookup}" and these are the top 3 results:\n')
             print_definitions(words_to_lookup)
         else:
             print(f'Did not recognize command {words_to_lookup} \n')
-
 
 get_input()
